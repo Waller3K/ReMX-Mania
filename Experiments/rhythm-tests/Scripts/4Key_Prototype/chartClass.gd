9d8d3c7f0b99b6c2d62b96101b4cfda915560e59
Waller3K <waltdraws@gmail.com>
2026-02-21 12:29:17 -0600

    Began work on prototype! - Added new audio tracks, began adding test track

---
+++ Experiments/rhythm-tests/Scripts/4Key_Prototype/chartClass.gd
@@ -0,0 +1,38 @@
+extends Node
+
+class_name Chart
+
+# Song Metadata Variables
+
+var bpm: 		float
+var songPath: 	String
+var songName: 	String
+var songArtist:	String
+var charter: String
+var difficulty:	float
+
+# Chart Metadata Variables
+var track1Path: String # the paths to the stems controlled by each track
+var track2Path: String
+var track3Path: String
+var track4Path: String
+var scratchTrackPaths = [] # an array of sample paths used by scratch track
+
+# Note dictionary which will contain the individual track's notes in arrays
+var notes = {
+	track1 = [],
+	track2 = [],
+	track3 = [],
+	track4 = [],
+	trackFX = [],
+	trackScratch = []
+}
+
+##########################################################
+# This function takes in a path to a JSON file 
+# and initializes all of the chart values so that they can
+# be used by other scripts. Returns true if it was sucessful,
+# and false on failure
+##########################################################
+func load(path: String) -> bool:
+	return true
