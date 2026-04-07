d7741a44a4c727335456fb275498cd37ed8af344
Walter Barlow <waltdraws@gmail.com>
2026-04-07 15:07:55 -0500

    Finished up early prototype - created final project file structure

--- Experiments/rhythm-tests/Scripts/4Key_Prototype/gameObjects/note.gd
+++ Experiments/rhythm-tests/Scripts/4Key_Prototype/gameObjects/note.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends TextureRect
 
 var targetTime: float
 var trackID: int
--- Experiments/rhythm-tests/Scripts/4Key_Prototype/noteManager.gd
+++ Experiments/rhythm-tests/Scripts/4Key_Prototype/noteManager.gd
@@ -25,66 +25,70 @@ func _onChartCreation(chart: Chart) -> void:
 	
 	var trackWidth = judegementLine.size.x/4
 	
+	#This line will give us the Track Colorrects in the Tracks HBoxContainer
+	var UITracks = judegementLine.get_child(0).get_child(0).get_children();
+	
 	# Preload the first bit of the map
+	# Fixed so that now it renders with the UI elements
+	# As a child of the track it is on
 	for i in range(noteData.track1.size()):
 		var newNote = noteScene.instantiate()
-		var noteXOffset = newNote.texture.get_width()/2
 		newNote.INIT(
 			GE.inputEnum.TRACK1, 
 			i, 
 			noteData.track1[i]["Pos"], 
 			spawningOffset, 
-			Vector2i(judegementLine.position.x + noteXOffset, -100), 
-			Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
+			Vector2i(UITracks[0].position.x, -100), 
+			Vector2(UITracks[0].position.x, UITracks[0].position.y + UITracks[0].size.y)
 		)
 		$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
-		add_child(newNote)
+		UITracks[0].add_child(newNote)
 		notes.track1.append(newNote)
 	
 	for i in range(noteData.track2.size()):
 		var newNote = noteScene.instantiate()
-		var noteXOffset = newNote.texture.get_width()/2
+		UITracks[1].add_child(newNote)
 		newNote.INIT(
 			GE.inputEnum.TRACK2, 
 			i, 
 			noteData.track2[i]["Pos"], 
 			spawningOffset, 
-			Vector2i(judegementLine.position.x + trackWidth + noteXOffset, -100),  
-			Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
+			Vector2i(UITracks[1].position.x, -100), 
+			Vector2(UITracks[1].position.x, UITracks[1].position.y + UITracks[1].size.y)
 		)
 		$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
-		add_child(newNote)
+		UITracks[1].add_child(newNote)
 		notes.track2.append(newNote)
 	
-	for i in range(noteData.track3.size()):
-		var newNote = noteScene.instantiate()
-		var noteXOffset = newNote.texture.get_width()/2
-		newNote.INIT(
-			GE.inputEnum.TRACK3, 
-			i, 
-			noteData.track3[i]["Pos"], 
-			spawningOffset, 
-			Vector2i(judegementLine.position.x + trackWidth * 2 + noteXOffset, -100),  
-			Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
-		)
-		$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
-		add_child(newNote)
-		notes.track3.append(newNote)
-	
-	for i in range(noteData.track4.size()):
-		var newNote = noteScene.instantiate()
-		var noteXOffset = newNote.texture.get_width()/2
-		newNote.INIT(
-			GE.inputEnum.TRACK4, 
-			i, 
-			noteData.track4[i]["Pos"], 
-			spawningOffset, 
-			Vector2i(judegementLine.position.x + trackWidth * 3 + noteXOffset, -100),  
-			Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
-		)
-		$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
-		add_child(newNote)
-		notes.track4.append(newNote)
+	#for i in range(noteData.track3.size()):
+		#var newNote = noteScene.instantiate()
+		#var noteXOffset = newNote.texture.get_width()/2
+		#newNote.INIT(
+			#GE.inputEnum.TRACK3, 
+			#i, 
+			#noteData.track3[i]["Pos"], 
+			#spawningOffset, 
+			#Vector2i(judegementLine.position.x + trackWidth * 2 + noteXOffset, -100),  
+			#Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
+		#)
+		#$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
+		#add_child(newNote)
+		#notes.track3.append(newNote)
+	#
+	#for i in range(noteData.track4.size()):
+		#var newNote = noteScene.instantiate()
+		#var noteXOffset = newNote.texture.get_width()/2
+		#newNote.INIT(
+			#GE.inputEnum.TRACK4, 
+			#i, 
+			#noteData.track4[i]["Pos"], 
+			#spawningOffset, 
+			#Vector2i(judegementLine.position.x + trackWidth * 3 + noteXOffset, -100),  
+			#Vector2(judegementLine.position.x, judegementLine.position.y + judegementLine.size.y * 0.5)
+		#)
+		#$"../AudioStreamPlayer".connect("songUpdate", newNote._onSongUpdate)
+		#add_child(newNote)
+		#notes.track4.append(newNote)
 
 func _onNoteHit(track, note):
 	match track:
