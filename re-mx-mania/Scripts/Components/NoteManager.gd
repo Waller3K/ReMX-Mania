7ad5a8877820ad72c12b3b6388a6e9a44941057c
Sen Le <senle@150-253-28-230.wifi.mcad.edu>
2026-09-03 19:35:57 -0500

    help

--- re-mx-mania/Scripts/Components/NoteManager.gd
+++ re-mx-mania/Scripts/Components/NoteManager.gd
@@ -203,4 +203,4 @@ func _onScratchHit(judgement: int, offset: float, noteIndex: int, subnoteIndex: 
 			hitNote.deactivate()
 			inactiveSubnotes.push_front(hitNote)
 			activeSubnotes.erase(hitNote)
-## すみまsorry broski 助けて
+## すみまsorry broski 助けて ;-;
